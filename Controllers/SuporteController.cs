22832838b51f3d908d77066961df6ec0912f6abe
AlexSouza7 <alexsandro.souza7.sdumont@gmail.com>
2021-11-06 14:56:18 -0300

    Implementação da página de suporte

---
+++ Controllers/SuporteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pindorama.Auth;
+using Pindorama.Models;
+using Pindorama.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pindorama.Controllers
+{
+    public class SuporteController : Controller
+    {
+        AuthService auth;
+        GamesService service;
+        public SuporteController(AuthService auth, GamesService service)
+        {
+            this.auth = auth;
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            if (auth.ValidateToken())
+            {
+                Token token = auth.GetCurrentToken();
+                ViewBag.currentUser = auth.GetCurrentToken();
+                return View(service.GetAllOwnedGames(token));
+            }
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
