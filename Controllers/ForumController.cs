dcb0ea98a53acd0c7ba51f470afd686b59338c9c
AlexSouza7 <alexsandro.souza7.sdumont@gmail.com>
2021-11-21 13:52:41 -0300

    Implementação do fórum

---
+++ Controllers/ForumController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pindorama.Auth;
+using Pindorama.Models;
+using Pindorama.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pindorama.Controllers
+{
+    public class ForumController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
